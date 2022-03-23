7f0e28c7861a3085e7062295eb43b0df2e4ef478
Amanda Gawlowicz <agawlowicz07@gmail.com>
2022-03-23 09:20:04 -0500

    Guess random number and what's your favorite subject

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.Next(1, 1000);
+            Console.WriteLine("Try to guess my favorite number.");
+            var userInput = int.Parse(Console.ReadLine());
+
+            //how to get the program to take input for a second guess?
+            while (favNumber != userInput)
+            {
+                if (userInput < favNumber)
+                {
+                    Console.WriteLine("too low");
+                }
+                else if (userInput > favNumber)
+                {
+                    Console.WriteLine("too high");
+                }
+                else
+                {
+                    Console.WriteLine("Nevermind");
+                }
+                Console.WriteLine("try again");
+                userInput = int.Parse(Console.ReadLine());
+
+            }
+
+            Console.WriteLine("good job! You guessed it!");
+
+            Console.WriteLine("What is your favorite school subject?");
+            var subject = Console.ReadLine().ToLower();
+
+            switch (subject)
+            {
+                case "history":
+                    Console.WriteLine("History is fascinating. We must learn about history so we don't repeat it.");
+                    break;
+                case "math":
+                    Console.WriteLine("Math is the foundation for understanding the world around us. Everything can be simplified into math.");
+                    break;
+                case "english":
+                    Console.WriteLine("English is a complex language and very intricate.");
+                    break;
+                case "biology":
+                    Console.WriteLine("Biology helps us understand the ways in which all life is connected.");
+                    break;
+                case "chemistry":
+                    Console.WriteLine("Chemistry is a very challenging subject.");
+                    break;
+
+            }
+        }
+    }
+}
